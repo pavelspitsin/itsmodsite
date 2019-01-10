754b69715f65fb5e7249e2fdf3fcb2e6cd4db721
Pavel <pavel.spitsin@yandex.ru>
2019-01-10 17:15:10 +0300

    New site upload. Added file MoD AF Respawner

---
+++ scripts/site.js
@@ -0,0 +1,30 @@
+﻿
+
+$(document).ready(function() {
+	$('.gallery-item').magnificPopup({
+	  type: 'image',
+          closeOnContentClick: false,
+          closeBtnInside: false,
+          mainClass: 'mfp-with-zoom mfp-img-mobile',
+          image: {
+            verticalFit: true,
+            titleSrc: function(item) {
+            	var title = item.el.attr('title');
+            	if (title !== undefined) {
+          			return item.el.attr('title') + ' &middot; <a class="image-source-link" href="'+item.el.attr('data-source')+'" target="_blank">Image source</a>';
+            	}
+          		return '<a class="image-source-link" href="'+item.el.attr('href')+'" target="_blank">Image source</a>';
+            }
+          },
+          gallery: {
+            enabled: true
+          },
+          zoom: {
+            enabled: true,
+            duration: 350, // don't foget to change the duration also in CSS
+            opener: function(element) {
+              return element.find('img');
+            }
+          }
+	});
+});
